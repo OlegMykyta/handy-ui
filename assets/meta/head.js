10d27add28139a036495a04232228fc8819c34ab
Nick <blizniukov@gmail.com>
2025-03-05 16:04:05 +0200

    Add dynamic seo

--- assets/meta/head.js
+++ assets/meta/head.js
@@ -1,15 +1,64 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    head.title("My SEO Optimized Page");
-    function updateMetaTag(name, content) {
-        let meta = document.querySelector(`meta[name="${name}"]`);
-        if (!meta) {
-            meta = document.createElement("meta");
-            meta.name = name;
-            document.head.appendChild(meta);
-        }
-        meta.content = content;
-    }
+function updateMeta(name, content) {
+    let meta = document.querySelector(`meta[name="${name}"]`) || document.createElement("meta");
+    meta.name = name;
+    meta.content = content;
+    document.head.appendChild(meta);
+}
 
-    updateMetaTag("description", "This is an SEO-friendly page.");
-    updateMetaTag("keywords", "JavaScript, SEO, HTML, HeadJS");
-});
+function updateOG(property, content) {
+    let meta = document.querySelector(`meta[property="${property}"]`) || document.createElement("meta");
+    meta.setAttribute("property", property);
+    meta.setAttribute("content", content);
+    document.head.appendChild(meta);
+}
+
+async function updateTags(property) {
+    try {
+        const response = await fetch("https://strapi-production-9858.up.railway.app/api/seo?pLevel=5");
+        const data = await response.json();
+        if (data && data.data && data.data[property]) {
+            const metaHome = data.data[property];
+            document.title = metaHome.title;
+
+            if (metaHome.tags) {
+                const tags = metaHome.tags;
+
+                tags.forEach(tag => {
+                    if (tag.name.startsWith("og")) {
+                        updateOG(tag.name, tag.value);
+                    } else {
+                        updateMeta(tag.name, tag.value);
+                    }
+                });
+            }
+        }
+
+    } catch (error) {
+        console.error("Error fetching meta tags:", error);
+    }
+}
+
+function updateTagsLocal(model) {
+    try {
+        console.log(model);
+        if (model && model.seo && model.seo.tags) {
+            const metaHome = model.seo;
+            document.title = metaHome.title;
+
+            if (metaHome.tags) {
+                const tags = metaHome.tags;
+
+                tags.forEach(tag => {
+                    if (tag.name.startsWith("og")) {
+                        updateOG(tag.name, tag.value);
+                    } else {
+                        updateMeta(tag.name, tag.value);
+                    }
+                });
+            }
+        }
+
+    } catch (error) {
+        console.error("Error fetching meta tags:", error);
+    }
+}
